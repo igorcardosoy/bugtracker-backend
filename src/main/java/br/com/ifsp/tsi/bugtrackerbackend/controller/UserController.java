a0dab2aae3a175e8583fd72051243fc56175f585
Igor Cardoso <igorfilipicardoso@gmail.com>
2025-06-18 19:28:36 -0300

    feat: add role-based access control to UserController and initialize roles in database

--- src/main/java/br/com/ifsp/tsi/bugtrackerbackend/controller/UserController.java
+++ src/main/java/br/com/ifsp/tsi/bugtrackerbackend/controller/UserController.java
@@ -12,6 +12,7 @@ import br.com.ifsp.tsi.bugtrackerbackend.model.entity.User;
 import br.com.ifsp.tsi.bugtrackerbackend.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/bugtracker/users")
+@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 public class UserController {
 
     private final UserService userService;
